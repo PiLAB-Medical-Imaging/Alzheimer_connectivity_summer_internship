6090c4d7cfb1c059d7e90d6a42fb42b41351c9d4
WILLOWMALAK <s.willowmalak@gmail.com>
2025-12-17 14:22:36 +0100

    Added the analysis pipeline

---
+++ analysis_pipeline.py
@@ -0,0 +1,35 @@
+"""Script to collect network metrics and build a dataset"""
+
+# Imports
+import numpy as np
+import pandas as pd
+import os
+import json
+
+
+def retrieve_participant_data(root_direc, subj_id):
+    network_types = ["structural", "functional", "combined"]
+
+    participant_data = {}
+
+    # Iterate through and capture participant data. 
+    for network in network_types:
+        directory = os.path.join(root_direc, subj_id, network, "networks")
+        for file_name in os.listdir(directory):
+            if file_name.__contains__("json"):
+                with open("data.json", "r", encoding="utf-8") as f:
+                    data = json.load(f)
+
+                participant_data[f"{network}_{file_name.split(".")[0]}"] = data
+
+    return participant_data
+
+def build_dataset(root_directory, patient_list_json_path):
+    with open(patient_list_json_path, "r") as f:
+        patient_list = json.load(f)
+
+    for patient in patient_list:
+        print(patient)
+        patient_data = retrieve_participant_data(root_directory, patient)
+        
+
