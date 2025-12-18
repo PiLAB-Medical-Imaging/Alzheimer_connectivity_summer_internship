bfffc5311fcd3c2821566252f43b6b669976bab4
WILLOWMALAK <s.willowmalak@gmail.com>
2025-12-18 16:14:38 +0100

    add debugging prints

--- analysis_pipeline.py
+++ analysis_pipeline.py
@@ -123,11 +123,16 @@ def simple_plotting(long_data):
     network_type = "structural"
     network_metric ="m_connectivity"
     network = "ecn"
-    data_subset = long_data.loc[ (long_data["type"]==network_type) & (long_data["network"]==network_metric)]
+    data_subset = long_data.loc[ (long_data["type"]==network_type) & (long_data["network"]==network)]
     x_values = data_subset[network_metric]
+
+    print("Debugging")
+    print(x_values)
+
     fig, axes = plt.subplots(len(NETWORK_TYPES), len(CURRENT_METRICS), figsize=(24, 7.5))
     for idx, metric in enumerate(CURRENT_METRICS):
-        axes[0,idx].plot(x_values, data_subset[metric])
+        y_values = data_subset[metric]
+        axes[0,idx].plot(x_values, y_values)
         axes[0, idx].set_title(f"{metric} vs {network_metric}")
     
     return fig
