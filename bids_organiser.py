589543052ee531ad848565de5e6ec98a446162fb
WILLOWMALAK <s.willowmalak@gmail.com>
2025-12-23 15:51:30 +0100

    bug hunting

--- bids_organiser.py
+++ bids_organiser.py
@@ -120,6 +120,8 @@ def bids_organise(data_folder, destination_folder, data_type, study_name, task="
 
         if len(subject_num)!=3:
             raise Exception(f"The participant number is not correctly formatted: \n Filename: {filename}\n participant number: {subject_num}")
+        if subject_num is None:
+            raise Exception(f"Subject number is None:\n Filename: {filename}\n participant number: {subject_num}")
 
 
         session_num = extract_session_number(filename)
