9eb58bc2e7b87e70ace96aa48dd7cf844b649589
WILLOWMALAK <s.willowmalak@gmail.com>
2026-02-04 14:09:04 +0100

    Minor tweak to register atlases code

--- run_steps.py
+++ run_steps.py
@@ -6,11 +6,17 @@ if __name__ == "__main__":
     tractography_folder = sys.argv[1]
     subj_line = sys.argv[2]
     template_file = sys.argv[3]
+    output_folder = sys.argv[4]
+    atlas_fp = sys.argv[5]
+    output_folder = sys.argv[6]
+
 
     register_atlases(
         fmri_prep_derivatives=fmri_prep_derivatives_folder,
         tractography_folder=tractography_folder,
         subject_line=subj_line,
-        template_file=template_file
+        template_file=template_file,
+        atlas_fp=atlas_fp,
+        output_folder=output_folder
     )
 
