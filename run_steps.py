5e671b49c6f2c2027ffae6c04d953be8b918f99f
WILLOWMALAK <s.willowmalak@gmail.com>
2026-02-04 14:27:52 +0100

    Added more prints

--- run_steps.py
+++ run_steps.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     template_file = sys.argv[3]
     output_folder = sys.argv[4]
     atlas_fp = sys.argv[5]
-
+    print(f"Sanity check\nLength of arguments {len(sys.argv)}")
     print(f"{fmri_prep_derivatives_folder}\n"
           f"{subj_line}\n"
           f"{template_file}\n"
