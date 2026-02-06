9f48f5b5bd21b637400c357cbb3285e6b0a76627
WILLOWMALAK <s.willowmalak@gmail.com>
2026-02-06 11:08:26 +0100

    Removed spurious argument

--- run_steps.py
+++ run_steps.py
@@ -80,8 +80,7 @@ if __name__ == "__main__":
     ppl.run_sc_matrix(
         subj_id=id_val+num,
         session=session,
-        output_folder=output_folder,
-        tractogram_file=tractogram_file
+        output_folder=output_folder
     )
     ppl.run_simple_weighting(
         subj_id=id_val+num,
